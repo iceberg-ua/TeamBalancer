8f54ac1f8ca78bb5a570773fbde715caf6a87532
iceberg-ua <wladimir.melnik@gmail.com>
2025-10-12 21:27:29 +0200

    add maui project

---
+++ TeamBalancer.Desktop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TeamBalancer.Desktop;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(new MainPage()) { Title = "TeamBalancer.Desktop" };
+    }
+}
---
+++ TeamBalancer.Desktop/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TeamBalancer.Desktop;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
