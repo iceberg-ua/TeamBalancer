17e9d4089036c52a3afcbfcd29e862039ecb0706
iceberg-ua <wladimir.melnik@gmail.com>
2026-01-11 13:32:29 +0100

    Refactor code structure for improved readability and maintainability

--- TeamBalancer/App.xaml.cs
+++ TeamBalancer/App.xaml.cs
@@ -9,6 +9,6 @@ public partial class App : Application
 
 	protected override Window CreateWindow(IActivationState? activationState)
 	{
-		return new Window(new MainPage()) { Title = "TeamBalancer" };
+		return new Window(new MainPage()) { Title = "Team Balancer" };
 	}
 }
