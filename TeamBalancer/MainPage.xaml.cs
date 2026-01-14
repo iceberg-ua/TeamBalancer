21eac84955f665c73a009dc10e37e1e71301820f
iceberg-ua <wladimir.melnik@gmail.com>
2026-01-14 21:14:33 +0100

    Refactor MainPage layout, add status bar spacer, and update AndroidManifest for improved UI

--- TeamBalancer/MainPage.xaml.cs
+++ TeamBalancer/MainPage.xaml.cs
@@ -5,5 +5,23 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+
+#if ANDROID
+		// Set status bar spacer height to match Android status bar
+		Microsoft.Maui.ApplicationModel.MainThread.BeginInvokeOnMainThread(() =>
+		{
+			var activity = Platform.CurrentActivity;
+			if (activity != null)
+			{
+				var resourceId = activity.Resources?.GetIdentifier("status_bar_height", "dimen", "android");
+				if (resourceId.HasValue && resourceId.Value > 0)
+				{
+					var statusBarHeight = activity.Resources?.GetDimensionPixelSize(resourceId.Value) ?? 0;
+					var density = activity.Resources?.DisplayMetrics?.Density ?? 1;
+					StatusBarSpacer.HeightRequest = statusBarHeight / density;
+				}
+			}
+		});
+#endif
 	}
 }
