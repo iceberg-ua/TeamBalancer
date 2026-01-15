092ac558d1c9ea8e71cc84f0b16e79afba3c3887
iceberg-ua <wladimir.melnik@gmail.com>
2026-01-15 11:46:28 +0100

    Refactor layout styles, add header spacer, and update splash screen colors for improved UI consistency

--- TeamBalancer/MainPage.xaml.cs
+++ TeamBalancer/MainPage.xaml.cs
@@ -5,23 +5,14 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		SetStatusBarSpacerHeight();
+	}
 
+	private void SetStatusBarSpacerHeight()
+	{
 #if ANDROID
-		// Set status bar spacer height to match Android status bar
-		Microsoft.Maui.ApplicationModel.MainThread.BeginInvokeOnMainThread(() =>
-		{
-			var activity = Platform.CurrentActivity;
-			if (activity != null)
-			{
-				var resourceId = activity.Resources?.GetIdentifier("status_bar_height", "dimen", "android");
-				if (resourceId.HasValue && resourceId.Value > 0)
-				{
-					var statusBarHeight = activity.Resources?.GetDimensionPixelSize(resourceId.Value) ?? 0;
-					var density = activity.Resources?.DisplayMetrics?.Density ?? 1;
-					StatusBarSpacer.HeightRequest = statusBarHeight / density;
-				}
-			}
-		});
+		// Minimal spacer - just enough to clear the status bar
+		StatusBarSpacer.HeightRequest = 4;
 #endif
 	}
 }
---
+++ TeamBalancer/Platforms/Android/MainActivity.cs
@@ -0,0 +1,18 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace TeamBalancer;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+#pragma warning disable CA1422
+        Window?.SetStatusBarColor(Android.Graphics.Color.ParseColor("#1b6ec2"));
+#pragma warning restore CA1422
+    }
+}
